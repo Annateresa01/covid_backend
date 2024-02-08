3beb2ba896441d5455c1e557e076db6ea21814f0
Annateresa01 <shajianna013@gmail.com>
2024-02-08 15:33:28 +0530

    changed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app=express()
 
 app.use(express.json())
 app.use(cors())
-mongoose.connect("mongodb+srv://anna07:anna@cluster0.a7na4kg.mongodb.net/covpatientsDb?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://anna07:anna@cluster0.a7na4kg.mongodb.net/covpatientstiDb?retryWrites=true&w=majority",
 {
   useNewUrlParser:true
 }
